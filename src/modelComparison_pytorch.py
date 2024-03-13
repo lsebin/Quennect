40d5d36556e9feb317b31d993ebce87f314df317
lsebin <raynnelee97@gmail.com>
2024-03-13 11:15:39 -0400

    started LIME

--- src/modelComparison_pytorch.py
+++ src/modelComparison_pytorch.py
@@ -71,9 +71,65 @@ def prepare_data(args=get_args()):
                                                             random_state = seed)
     return X_train, X_test, y_train, y_test
 
-def run(args=get_args()):
-    # print(args.hidden, args.hidden1, args.hidden2)
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    correct , train_loss = 0, 0
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device).to(torch.float32), y.to(device)
+        y = y.squeeze()
+        pred = model(X)
+        # print(pred, y)
+        # print(pred.dtype, y.dtype)
+        loss = loss_fn(pred, y)
+        _, lbls = torch.max(pred.data, 1)
+        correct += (lbls == y).type(torch.float).sum().item() 
+        # print(pred, y)
+        
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward() 
+        optimizer.step()
+        train_loss += (loss.item())
+
+    train_loss /= num_batches 
+    correct /= size 
+    print(f"Train: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {train_loss:>8f} \n")
+    return correct, train_loss
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    model.eval() # parameters no update
+    with torch.no_grad(): # disable gradient calculation
+        for X, y in dataloader:
+            X, y = X.to(device).to(torch.float32), y.to(device)
+            y = y.squeeze()
+            pred = model(X)
+            # print(outputs,pred, y)
+
+            _, lbls = torch.max(pred.data, 1)
+            correct += (lbls == y).type(torch.float).sum().item() 
+            loss = loss_fn(pred, y)
+            test_loss += loss.item() 
+    test_loss /= num_batches      
+    correct /= size # the overall accuracy 
+    print(f"Test: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return correct, test_loss
+
+
+if __name__ == "__main__":
+
+    args=get_args()
+    
+    epochs = 5 #args.epoch
+    
+    # filepath = os.path.join("model", f"epoch{epochs}.pt")
+    # the_model = torch.load(filepath)
     # exit()
+    
     X_train, X_test, y_train, y_test = prepare_data()
     training_data = Q_vecDataset(X_rus = X_train, y_rus = y_train, train=True)
     test_data = Q_vecDataset(X_rus = X_test, y_rus = y_test, train=False)
@@ -145,73 +201,27 @@ def run(args=get_args()):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr) # lr = learning rate
 
-    def train(dataloader, model, loss_fn, optimizer):
-        size = len(dataloader.dataset)
-        num_batches = len(dataloader)
-        correct , train_loss = 0, 0
-        model.train()
-        for batch, (X, y) in enumerate(dataloader):
-            X, y = X.to(device).to(torch.float32), y.to(device)
-            y = y.squeeze()
-            pred = model(X)
-            # print(pred, y)
-            # print(pred.dtype, y.dtype)
-            loss = loss_fn(pred, y)
-            _, lbls = torch.max(pred.data, 1)
-            correct += (lbls == y).type(torch.float).sum().item() 
-            # print(pred, y)
-            
-            # Backpropagation
-            optimizer.zero_grad()
-            loss.backward() 
-            optimizer.step()
-            train_loss += (loss.item())
-
-        train_loss /= num_batches 
-        correct /= size 
-        print(f"Train: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {train_loss:>8f} \n")
-        return correct, train_loss
-
-        
-                
-    def test(dataloader, model, loss_fn):
-        size = len(dataloader.dataset)
-        num_batches = len(dataloader)
-        test_loss, correct = 0, 0
-        model.eval() # parameters no update
-        with torch.no_grad(): # disable gradient calculation
-            for X, y in dataloader:
-                X, y = X.to(device).to(torch.float32), y.to(device)
-                y = y.squeeze()
-                pred = model(X)
-                # print(outputs,pred, y)
-
-                _, lbls = torch.max(pred.data, 1)
-                correct += (lbls == y).type(torch.float).sum().item() 
-                loss = loss_fn(pred, y)
-                test_loss += loss.item() 
-        test_loss /= num_batches      
-        correct /= size # the overall accuracy 
-        print(f"Test: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-        return correct, test_loss
-        
-
     # one training epoch -> algo made one pass through the training dataset
-    epochs = 200 #args.epoch
     test_acc_list = []
     test_loss_list = []
     train_acc_list = []
     train_loss_list = []
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        train_acc, train_loss = train(train_dataloader, model, loss_fn, optimizer)
-        test_acc, test_loss= test(test_dataloader, model, loss_fn)
+        train_acc, train_loss = train(train_dataloader, model, loss_fn, optimizer, device)
+        test_acc, test_loss= test(test_dataloader, model, loss_fn, device)
         
         train_acc_list.append(train_acc)
         train_loss_list.append(train_loss)
         test_acc_list.append(test_acc)
         test_loss_list.append(test_loss) 
     print("Done!")
+    
+    filepath = os.path.join("model", f"epoch{epochs}.pt")
+    torch.save(model, filepath)
+    print("Saved!")
+    
+    exit()
     
     filepath = os.path.join("fig", "supervised")
     if not os.path.exists(filepath):
@@ -240,6 +250,9 @@ def run(args=get_args()):
     plt.savefig(os.path.join(filepath, f"loss_{epochs}.png"))
     
     print("Saved!")
-
-if __name__ == "__main__":
-    run()
+    
+    # filepath = os.path.join("model", "epoch5.pth")
+    # the_model = torch.load(filepath)
+    
+    # print(the_model)
+    # torch.load()
