0684fda54d486353f9361aeb2681c7c518e0cf72
shanetw4 <52422842+shanetw4@users.noreply.github.com>
2024-04-10 19:26:17 -0400

    contains fn that returns top features/shapvals

---
+++ src/run_model.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import argparse, os
+import numpy as np
+import matplotlib.pyplot as plt
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import KFold
+from sklearn.utils.class_weight import compute_class_weight
+from sklearn.metrics import f1_score
+import shap
+
+BATCH_SIZE = 100
+LR = 0.001
+HIDDEN1 = 64
+HIDDEN2 = 32
+
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+
+hidden1 = HIDDEN1
+hidden2 = HIDDEN2
+input_size = 30
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, hidden1)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc3 = nn.Linear(hidden2, 2)
+        self.sigmoid = nn.Sigmoid()
+        self.dropout = nn.Dropout(p=0.5)
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        return(x)
+
+def explain(point, X_train):
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load('model/model_full_state_dict_epoch1k.pth'))
+    model.eval()
+
+    ex_point = point
+    ex_numpy = np.expand_dims(ex_point.to_numpy(), axis=0)
+    ex_tensor = torch.tensor(ex_numpy, dtype = torch.float)
+
+    train_numpy = X_train.to_numpy()
+    train_tensor = torch.tensor(train_numpy, dtype = torch.float)
+
+    explainer = shap.GradientExplainer(model, train_tensor)
+    shap_values = explainer.shap_values(ex_tensor)[:,:,0]
+    shap.summary_plot(shap_values, ex_tensor, feature_names = X_train.columns)
+
+    shap_pd = pd.DataFrame(shap_values, columns = X_train.columns)
+    top_ind = np.argsort(shap_pd.values.flatten())[-5:][::-1]
+    top_features = shap_pd.columns[top_ind].to_numpy()
+    top_shap = shap_pd.values.flatten()[top_ind]
+    return top_features, top_shap
+
+if __name__ == "__main__":
+    X_train = pd.read_csv('data/X_train.csv')
+    X_train = X_train.drop('Unnamed: 0', axis=1)
+    top_neg_features, shap_vals = explain(X_train.iloc[0,:], X_train)
+    print(top_neg_features)
+    print(shap_vals)
