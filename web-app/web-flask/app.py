d0454d22e3ebf69f8de59f7343341e6794298117
salmary <112910438+salmary@users.noreply.github.com>
2024-04-11 17:07:01 -0400

    new build

--- web-app/web-flask/app.py
+++ web-app/web-flask/app.py
@@ -3,9 +3,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "home"
+
 @app.route('/api/model')
-def get_current_time():
-    return {'time': time.time()}
+def model():
+    return "model"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(debug=True)
