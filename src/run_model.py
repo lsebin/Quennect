e808134647b1aee6301f2c423d71cbbcfccaf636
lsebin <raynnelee97@gmail.com>
2024-04-11 14:46:34 -0400

    restructured

--- src/run_model.py
+++ src/run_model.py
@@ -6,41 +6,41 @@ import shap
 import joblib
 from run_lime import scale_user_input
 
-BATCH_SIZE = 100
-LR = 0.001
-HIDDEN1 = 64
-HIDDEN2 = 32
+def run_shap(point, X_train):
+    
+    BATCH_SIZE = 100
+    LR = 0.001
+    HIDDEN1 = 64
+    HIDDEN2 = 32
 
-device = 'cpu'
-if torch.cuda.is_available():
-    device = 'cuda'
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
 
-hidden1 = HIDDEN1
-hidden2 = HIDDEN2
-input_size = 30
+    hidden1 = HIDDEN1
+    hidden2 = HIDDEN2
+    input_size = 30
 
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(input_size, hidden1)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden1, hidden2)
-        self.fc3 = nn.Linear(hidden2, 2)
-        self.sigmoid = nn.Sigmoid()
-        self.dropout = nn.Dropout(p=0.5)
-        self.flatten = nn.Flatten()
+    class NeuralNetwork(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.fc1 = nn.Linear(input_size, hidden1)
+            self.relu = nn.ReLU()
+            self.fc2 = nn.Linear(hidden1, hidden2)
+            self.fc3 = nn.Linear(hidden2, 2)
+            self.sigmoid = nn.Sigmoid()
+            self.dropout = nn.Dropout(p=0.5)
+            self.flatten = nn.Flatten()
 
-    def forward(self, x):
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
-        return(x)
-
-def explain(point, X_train):
+        def forward(self, x):
+            x = self.flatten(x)
+            x = self.fc1(x)
+            x = self.relu(x)
+            x = self.dropout(x)
+            x = self.fc2(x)
+            x = self.relu(x)
+            x = self.fc3(x)
+            return(x)
     model = NeuralNetwork()
     model.load_state_dict(torch.load('model/model_full_state_dict_epoch1k.pth'))
     model.eval()
@@ -76,5 +76,5 @@ if __name__ == "__main__":
     user_input = scale_user_input(user_input)
     X_test = pd.Series((x for x in user_input[0]))
     
-    top_5_features_rec = explain(X_test, X_train)
+    top_5_features_rec = run_shap(X_test, X_train)
     print(top_5_features_rec)
