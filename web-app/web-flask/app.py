9c0b1f24c2ce1dfc02446ef7969b7d0f437d62b3
salmary <112910438+salmary@users.noreply.github.com>
2024-04-11 13:46:00 -0400

    fix

--- web-app/web-flask/app.py
+++ web-app/web-flask/app.py
@@ -1,3 +1,11 @@
-from flask import Flask
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/api/model')
+def get_current_time():
+    return {'time': time.time()}
+
+if __name__ == "__main__":
+app.run(port=5000)
